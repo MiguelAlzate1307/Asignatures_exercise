2727690b20b3d51bec1be567af824001464581a1
Miguel Alzate <alzatemiguel284@gmail.com>
2024-10-23 23:12:52 -0500

    Initial commit

---
+++ src/main/java/co/edu/umanizales/asignatures_exercise/AsignaturesExerciseApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.asignatures_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignaturesExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignaturesExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/umanizales/asignatures_exercise/AsignaturesExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.asignatures_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsignaturesExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
